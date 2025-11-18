eec7092d8a27a9d55c2613da3036f133207b4336
AUGUSTO CONTE CARGNIN TEIXEIRA <augusto.teixeira@univille.br>
2025-11-18 20:14:24 -0300

    Fazendo ajustes no DB adicionando e colocando herança de classes, fazendo condição para cadastro tipo Roupa/Acessorio, e começando a fazer condição de tipo de produto Roupa/Acessorio

--- app.py
+++ app.py
@@ -8,13 +8,31 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Roupa(db.Model):
+class Produto(db.Model):
+    __tablename__ = 'produtos'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    tamanho = db.Column(db.String(10))
     preco = db.Column(db.Float)
     criado_em = db.Column(db.DateTime, default=db.func.current_timestamp())
     editado_em = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    tipo = db.Column(db.String(50))  # Campo para discriminar o tipo de produto
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'produto',
+        'polymorphic_on': tipo
+    }
+
+class Roupa(Produto):
+    __mapper_args__ = {
+        'polymorphic_identity': 'roupa',
+    }
+    tamanho = db.Column(db.String(10))
+
+class Acessorios(Produto):
+    __mapper_args__ = {
+        'polymorphic_identity': 'acessorio',
+    }
+    tipo_acessorio = db.Column(db.String(50))
 
 @app.route('/')
 def main():
@@ -30,8 +48,8 @@ def cadastro():
 
 @app.route('/editar')
 def editar():
-    roupas = Roupa.query.all()
-    return render_template('editar.html', roupas=roupas)
+    produtos = Produto.query.all()
+    return render_template('editar.html', produtos = produtos)
 
 @app.route('/add', methods=['POST'])
 def add_roupa():
@@ -77,6 +95,25 @@ def delete_roupa(id):
     db.session.delete(roupa)
     db.session.commit()
     return redirect(url_for('main'))
+
+@app.route('/add_acessorio', methods=['POST'])
+def add_acessorio():
+    nome = request.form.get('nome')
+    tipo_acessorio = request.form.get('tipo_acessorio')
+    preco = request.form.get('preco')
+    preco_limpo = preco.replace('R$', '').replace('.', '').replace(',', '.').strip()
+    preco_float = float(preco_limpo)
+
+    novo_acessorio = Acessorios(
+        nome=nome,
+        tipo_acessorio=tipo_acessorio,
+        preco=preco_float,
+        criado_em=db.func.current_timestamp(),
+        editado_em=db.func.current_timestamp()
+    )
+    db.session.add(novo_acessorio)
+    db.session.commit()
+    return redirect(url_for('cadastro', sucesso=True))
     
 if __name__ == '__main__':
     with app.app_context():
